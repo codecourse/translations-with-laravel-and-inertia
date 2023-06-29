b722d87de80aa86d91469694858ed27d982d61aa
Alex Garrett-Smith <alex@codecourse.com>
2023-06-29 10:36:12 +0100

    Episode 09

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,13 +41,15 @@ class HandleInertiaRequests extends Middleware
             'language' => app()->getLocale(),
             'languages' => LanguageResource::collection(Lang::cases()),
             'translations' => function () {
-                return collect(File::allFiles(base_path('lang/' . app()->getLocale())))
-                    ->flatMap(function ($file) {
-                        return Arr::dot(
-                            File::getRequire($file->getRealPath()),
-                            $file->getBasename('.' . $file->getExtension()) . '.'
-                        );
-                    });
+                return cache()->rememberForever('translations.' . app()->getLocale(), function () {
+                    return collect(File::allFiles(base_path('lang/' . app()->getLocale())))
+                        ->flatMap(function ($file) {
+                            return Arr::dot(
+                                File::getRequire($file->getRealPath()),
+                                $file->getBasename('.' . $file->getExtension()) . '.'
+                            );
+                        });
+                });
             },
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
