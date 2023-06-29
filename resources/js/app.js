0e0c5304179a122ea76c7a2223ac0520e2e0f87c
Alex Garrett-Smith <alex@codecourse.com>
2023-06-29 10:31:03 +0100

    Episode 08

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import __ from './lang';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,10 +13,13 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
-            .mount(el);
+
+        app.config.globalProperties.__ = __
+
+        app.mount(el);
     },
     progress: {
         color: '#4B5563',
---
+++ resources/js/lang.js
@@ -0,0 +1,11 @@
+import { usePage } from '@inertiajs/vue3'
+
+export default function __ (key, replacements = {}) {
+    let translation = usePage().props.translations[key] || key
+
+    Object.keys(replacements).forEach(replacement => {
+        translation = translation.replace(`:${replacement}`, replacements[replacement])
+    })
+
+    return translation
+}
